869a3766236417bfc7712e8cd7f776826a5a57b1
Sana Akhunji <113094148+sanaa9012@users.noreply.github.com>
2025-01-21 14:52:40 +0530

    done done done

--- functions/add_car.py
+++ functions/add_car.py
@@ -5,8 +5,20 @@ import os
 from flask import request
 from sharedcode.db_connections import get_db_connection 
 from sharedcode.query import add_car_query
+from sharedcode.firebase_auth import FirebaseAuthService
 
 def add_car():
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify({"error": "Unauthorized"}), 401
+    print(f'requestsss: {token}')
+    firebase_auth_service = FirebaseAuthService()
+    uid = firebase_auth_service.verify_firebase_token(token)
+    if uid is None:
+        return jsonify({"message": "Authentication failed"}), 401
+    
+    print(f'uid: {uid}')
+    
     connection = get_db_connection()
     cursor = connection.cursor(cursor_factory=extras.RealDictCursor)
     
@@ -16,6 +28,9 @@ def add_car():
     year = data.get('year')
     color = data.get('color')
     user_id = data.get('user_id')
+    
+    if not all([brand, model, year, color, user_id]):
+        return jsonify({"error": "Missing required fields"}), 400
     
     cursor.execute(add_car_query, (brand, model, year, color, user_id))
     connection.commit()
--- functions/car_by_id.py
+++ functions/car_by_id.py
@@ -1,10 +1,22 @@
-from flask import jsonify
+from flask import jsonify, request
 import psycopg2
 from psycopg2 import extras
 from sharedcode.db_connections import get_db_connection
 from sharedcode.query import car_by_id_query
+from sharedcode.firebase_auth import FirebaseAuthService
 
 def get_car(user_id):
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify({"error": "Unauthorized"}), 401
+    print(f'requestsss: {token}')
+    firebase_auth_service = FirebaseAuthService()
+    uid = firebase_auth_service.verify_firebase_token(token)
+    if uid is None:
+        return jsonify({"message": "Authentication failed"}), 401
+    
+    print(f'uid: {uid}')    
+    
     connection = get_db_connection()
     cursor = connection.cursor(cursor_factory=extras.RealDictCursor)
     
